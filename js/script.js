83dd50630a47563ce8c14c0397fafe6098935e28
Emil5466 <71122773+Emil5466@users.noreply.github.com>
2021-05-28 05:27:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// owl carousel script
+$('.carousel').owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplayTimeOut: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+      0:{
+          items: 1,
+          nav: false
+      },
+      600:{
+          items: 2,
+          nav: false
+      },
+      1000:{
+          items: 3,
+          nav: false
+      }
+  }
+});
+
+
+// navbar 
+
+const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menu = document.querySelector(".menu-list");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      menu.classList.add("active");
+      menuBtn.classList.add("hide");
+      cancelBtn.classList.add("show");
+      body.classList.add("disabledScroll");
+    }
+    cancelBtn.onclick = ()=>{
+      menu.classList.remove("active");
+      menuBtn.classList.remove("hide");
+      cancelBtn.classList.remove("show");
+      body.classList.remove("disabledScroll");
+    }
+
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
+
---
+++ js/script2.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+    const navbar = document.querySelector(".main_nav");
+    const menu = document.querySelector(".inner_main_nav");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      menu.classList.add("active");
+      menuBtn.classList.add("hide");
+      cancelBtn.classList.add("show");
+      body.classList.add("disabledScroll");
+    }
+    cancelBtn.onclick = ()=>{
+      menu.classList.remove("active");
+      menuBtn.classList.remove("hide");
+      cancelBtn.classList.remove("show");
+      body.classList.remove("disabledScroll");
+    }
+
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
+
+    new Glide('.glide').mount()
